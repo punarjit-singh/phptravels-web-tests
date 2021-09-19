19720be99d53be1b37aa60c5a329d3ccb33ffd0a
Punarjit Singh <psingh22@woolworths.com.au>
2021-09-19 23:31:39 +1000

    config updates

--- .testcaferc.js
+++ .testcaferc.js
@@ -3,7 +3,7 @@ module.exports = {
     skipUncaughtErrors: true,
     stopOnFirstFail: true,
     quarantineMode: false,
-    browsers: "all",
+    browsers: "chrome",
     src: ["./tests/**/*.js"],
     userVariables: {
         baseUrl: "https://www.phptravels.net/",
